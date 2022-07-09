d8821570800851067a7be28c6b9c5eac4b2e61a9
surenravi1407 <surenravi1407@gmail.com>
2022-07-09 15:02:37 +0300

    cy.cmd

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
     <div>
-      <h1 className="text-center text-4xl">Arkhamm Go NFT Marketplace</h1>
+      <h1 className="text-center text-4xl">Arkhamm Go NFT Marketplaces</h1>
     </div>
   )
 }
